816fca8d66491f1555f0b8e6c8a37c456bc8106f
Jason Beres <jasonberes@gmail.com>
2024-12-02 09:34:16 -0500

    Add files via upload

---
+++ Reveal/DataSourceProvider.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reveal.Sdk;
+using Reveal.Sdk.Data;
+using Reveal.Sdk.Data.Csv;
+using Reveal.Sdk.Data.Microsoft.SqlServer;
+
+namespace RevealSdk.Server.Reveal
+{
+    internal class DataSourceProvider : IRVDataSourceProvider
+    { 
+        public Task<RVDataSourceItem>? ChangeDataSourceItemAsync(IRVUserContext userContext, 
+                string dashboardId, RVDataSourceItem dataSourceItem)
+        {
+            if (dataSourceItem is RVSqlServerDataSourceItem sqlDsi)
+            {
+                ChangeDataSourceAsync(userContext, sqlDsi.DataSource);
+
+                if (sqlDsi.Id == "CustOrderHist")
+                {
+                    sqlDsi.Procedure = "CustOrderHist";
+                    sqlDsi.ProcedureParameters = new Dictionary<string, object> { { "@CustomerID", userContext.UserId } };
+                }
+
+                else if (sqlDsi.Id == "CustOrdersOrders")
+                {
+                    sqlDsi.Procedure = "CustOrdersOrders";
+                    sqlDsi.ProcedureParameters = new Dictionary<string, object> { { "@CustomerID", userContext.UserId } };
+                }
+
+                else if (sqlDsi.Id == "TenMostExpensiveProducts")
+                {
+                    sqlDsi.Procedure = "Ten Most Expensive Products";
+                }
+
+                else if (sqlDsi.Id == "CustomerOrders")
+                {
+                    sqlDsi.CustomQuery = "Select * from Orders Where OrderId = " + userContext.Properties["OrderId"];
+                }
+
+                else if (sqlDsi.Id == "TenMostExpensiveProducts")
+                {
+                    sqlDsi.Title = "Ten Most Expensive Products";
+                    sqlDsi.Subtitle = "Whatever";
+                    sqlDsi.Procedure = "Ten Most Expensive Products";
+                }
+                else if (sqlDsi.Table == "OrdersQry")
+                {
+                    //sqlDsi.Database = "devtest";
+                }
+
+                //else return null;
+            }
+            return Task.FromResult(dataSourceItem);
+        }
+
+        public Task<RVDashboardDataSource> ChangeDataSourceAsync(IRVUserContext userContext, RVDashboardDataSource dataSource)
+        {
+            if (dataSource is RVSqlServerDataSource sqlDs)
+            {
+                sqlDs.Host = "10.20.8.57";
+                sqlDs.Database = "devtest";
+                //sqlDs.Schema = "venus";
+            }
+            return Task.FromResult(dataSource);
+        }
+    }
+}
---
+++ Reveal/UserContextProvider.cs
@@ -0,0 +1,29 @@
+﻿using Reveal.Sdk;
+
+namespace RevealSdk.Server.Reveal
+{
+    public class UserContextProvider : IRVUserContextProvider
+    {
+        IRVUserContext IRVUserContextProvider.GetUserContext(HttpContext aspnetContext)
+        {
+            var userId = aspnetContext.Request.Headers["x-header-one"];
+            var orderId = aspnetContext.Request.Headers["x-header-two"];
+            var employeeId = aspnetContext.Request.Headers["x-header-three"];
+
+            string role = "User";
+            if (userId == "AROUT" || userId == "BLONP")
+            {
+                role = "Admin";
+            }
+
+            var props = new Dictionary<string, object>() {
+                    { "OrderId", orderId },
+                    { "EmployeeId", employeeId },
+                    { "Role", role } };
+
+            Console.WriteLine("UserContextProvider: " + userId + " " + orderId + " " + employeeId);
+
+            return new RVUserContext(userId, props);
+        }
+    }
+}
